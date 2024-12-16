2201b37ace1c19f6dbabf2df3e697002561ddbf2
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2024-12-16 20:25:36 +0100

    Descripción de los cambios realizados

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,6 +1,23 @@
 export const footer = {
-    template: `footer`,
-    string: ()=>{
-      //logica javascript para el componente
-    } 
+    template: // html
+    `
+  <nav class="navbar bg-secondary fixed-bottom small">
+    <div class="container">
+      <a class="navbar-brand" href="http://www.fpllefia.com">
+        <img
+          src="../assets/images/logo (1).svg"
+          alt="fpllefia"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        FPLlefià
+      </a>
+      <span class="navbar-text">@Texto de header</span>
+      <a href="#" class="nav-link">Vínculo header</a>
+    </div>
+  </nav>
+    `
+    
   }
+  
